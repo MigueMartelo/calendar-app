e437b14c8d2dc9e8a773ba8a36d969ff58db9fad
Migue Martelo <miguemartelo1031@gmail.com>
2022-02-25 20:18:23 -0500

    FEAT: Aditional configuration big-calendar.

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -3,7 +3,12 @@ import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 
 import { Navbar } from '../ui/Navbar';
+import { messages } from '../../helpers/calendar-messages.es';
+
+import 'moment/locale/es';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+moment.locale('es');
 
 const localizer = momentLocalizer(moment);
 
@@ -17,6 +22,17 @@ const myEventsList = [
 ];
 
 export const CalendarScreen = () => {
+  const eventStyleGetter = (event, start, end, isSelected) => {
+    const style = {
+      backgroundColor: '#367cf7',
+      borderRadius: '0px',
+      opacity: 0.8,
+      display: 'block',
+      color: 'white',
+    };
+
+    return { style };
+  };
   return (
     <div className='calendar-screen'>
       <Navbar />
@@ -25,6 +41,8 @@ export const CalendarScreen = () => {
         events={myEventsList}
         startAccessor='start'
         endAccessor='end'
+        messages={messages}
+        eventPropGetter={eventStyleGetter}
       />
     </div>
   );
