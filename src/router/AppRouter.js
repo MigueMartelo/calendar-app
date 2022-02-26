a43db4245f1e1aafb561bd71318390506374a6c5
Migue Martelo <miguemartelo1031@gmail.com>
2022-02-25 19:45:04 -0500

    FEAT: Default route.

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { LoginScreen } from '../components/auth/LoginScreen';
 import { CalendarScreen } from '../components/calendar/CalendarScreen';
 
@@ -9,6 +9,7 @@ export const AppRouter = () => {
       <Routes>
         <Route path='/' exact element={<CalendarScreen />} />
         <Route path='/login' exact element={<LoginScreen />} />
+        <Route path='*' element={<Navigate to='/' />} />
       </Routes>
     </BrowserRouter>
   );
